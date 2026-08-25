8a03ad42b3c5e3f928c44f8bdd6e28134c1ede5a
sondos <sondoswaheed184@gmail.com>
2026-08-25 23:35:17 +0300

    Finish Notes Endpoint

--- phase-02-web-api-basics/task-01-rest-routing-drills/ApiRoutingDrills/ApiRoutingDrills/Controllers/NotesController.cs
+++ phase-02-web-api-basics/task-01-rest-routing-drills/ApiRoutingDrills/ApiRoutingDrills/Controllers/NotesController.cs
@@ -1,6 +1,7 @@
 ﻿using ApiRoutingDrills.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
 
 namespace ApiRoutingDrills.Controllers
 {
@@ -22,7 +23,75 @@ namespace ApiRoutingDrills.Controllers
             };
 
             Notes.Add(notes);
-            return StatusCode(200, notes);
+            return StatusCode(201, notes);
         } 
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Notes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var note= Notes.FirstOrDefault(i=>i.Id==id);
+            if(note == null)
+            {
+               return NotFound(new {message="Note not found"});
+            }
+            return Ok(note);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id , [FromBody] UpdateNoteRequest request)
+        {
+           var note =Notes.FirstOrDefault(i=>i.Id==id);
+            // not found 404 
+            if (note == null) { return NotFound(new { message = "Id doesn't exist" }); }
+
+            if (string.IsNullOrWhiteSpace(request.Title) || string.IsNullOrWhiteSpace(request.Content)) {
+                // status code 400
+                return BadRequest();
+            }
+            note.Title = request.Title;
+            note.Content = request.Content;
+
+            return Ok(
+                new { Updatednotes=note,
+                message="updated note returned"
+            });
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var note = Notes.FirstOrDefault(x=>x.Id==id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            Notes.Remove(note);
+            return NoContent();
+        }
+
+        [HttpGet("Search")]
+        public IActionResult Search([FromQuery] string keyword)
+        {
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                return BadRequest(new
+                {
+                    message = "Keyword is required"
+                });
+            }
+
+            var matchingNotes = Notes.Where(note =>note.Title.Contains(keyword, StringComparison.OrdinalIgnoreCase) ||
+                                             note.Content.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            return Ok(matchingNotes);
+        }
+        
     }
 }
---
+++ phase-02-web-api-basics/task-01-rest-routing-drills/ApiRoutingDrills/ApiRoutingDrills/DTOs/UpdateNoteRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiRoutingDrills.DTOs
+{
+    public class UpdateNoteRequest
+    {
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Content { get; set; }
+    }
+}
