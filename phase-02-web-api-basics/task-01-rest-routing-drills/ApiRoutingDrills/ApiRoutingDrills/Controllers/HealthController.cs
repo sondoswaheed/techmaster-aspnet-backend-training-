05d69481d49a9ea4955d6da72a052dc40ff93977
sondos <sondoswaheed184@gmail.com>
2026-08-25 20:26:45 +0300

    Task00-01

---
+++ phase-02-web-api-basics/task-01-rest-routing-drills/ApiRoutingDrills/ApiRoutingDrills/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRoutingDrills.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "Running", service = "TechMaster API", time = DateTime.UtcNow });
+        }
+
+    }
+}
