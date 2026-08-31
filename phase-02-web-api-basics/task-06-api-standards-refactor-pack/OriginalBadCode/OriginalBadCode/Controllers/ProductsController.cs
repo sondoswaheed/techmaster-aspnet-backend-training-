ed27622a5fed2e9eae711c7c70d7b8eb4c20c2d1
sondos <sondoswaheed184@gmail.com>
2026-08-31 13:38:22 +0300

    original code before refactor

---
+++ phase-02-web-api-basics/task-06-api-standards-refactor-pack/OriginalBadCode/OriginalBadCode/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OriginalBadCode.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        static List<Product> products = new List<Product>();
+        [HttpPost]
+        public IActionResult Add(string name, decimal price, int stock)
+        {
+            if (name == "")
+            {
+                return Ok("bad name");
+            }
+            if (price < 0)
+            {
+                return Ok("bad price");
+            }
+            var p = new Product();
+            p.Id = products.Count + 1;
+            p.Name = name;
+            p.Price = price;
+            p.Stock = stock;
+            products.Add(p);
+            return Ok(p);
+        }
+        [HttpGet("all")]
+        public IActionResult All()
+        {
+            return Ok(products);
+        }
+        [HttpGet("get")]
+        public IActionResult Get(int id)
+        {
+            foreach (var p in products)
+            {
+                if (p.Id == id)
+                {
+                    return Ok(p);
+                }
+            }
+            return Ok("not found");
+        }
+    }
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public decimal Price;
+        public int Stock;
+    }
+}
